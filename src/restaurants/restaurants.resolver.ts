55f8ab0264656586433407d458be0bf0bdd1e4d2
heecheol153 <heecheol153@gmail.com>
2021-06-30 01:04:59 +0900

    #1.5 Validating ArgsTypes

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -6,12 +6,10 @@ import { Restaurant } from './entities/restaurant.entity';
 export class RestaurantResolver {
   @Query(returns => [Restaurant])
   restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    console.log(veganOnly);
     return [];
   }
   @Mutation(returns => Boolean)
   createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
-    console.log(createRestaurantDto);
     return true;
   }
 }
